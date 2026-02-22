e63d27142e51c6640266ffd01343fdf43beb0085
Adham mohammed <159294391+adham-labeb@users.noreply.github.com>
2026-02-22 15:07:00 +0200

    Update and rename Cross validation with Z-score.py to old code/Cross validation with Z-score.py

--- old code/Cross validation with Z-score.py
+++ old code/Cross validation with Z-score.py
@@ -1,20 +1,30 @@
 #### Importing the libraries ####
+import pandas as pd
 import numpy as np
 import glob
 import cv2
+import time
+from statistics import mean
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier 
+from sklearn.naive_bayes import GaussianNB 
+from sklearn.neural_network import MLPClassifier 
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_validate 
+from sklearn.metrics import accuracy_score, recall_score, precision_score
 
 ###########################     Data Input     ####################################
 X = []
 y = []
 
-for file in glob.glob('C:\images/*.png'):
+for file in glob.glob('Normal/*.png'):
     images = cv2.imread(file,0)
     Resize_image = cv2.resize(images, dsize=(256, 256), interpolation=cv2.INTER_CUBIC)
     Reshape_image=Resize_image.reshape(1,256*256)
     X.append(Reshape_image)
     y.append(0)
 
-for file in glob.glob('C:\images-covid/*.png'):
+for file in glob.glob('COVID/*.png'):
     images = cv2.imread(file,0)
     Resize_image = cv2.resize(images, dsize=(256, 256), interpolation=cv2.INTER_CUBIC)
     Reshape_image=Resize_image.reshape(1,256*256)
@@ -30,29 +40,24 @@ y = np.array(y)
 y = y.reshape(y.shape[0],1)
 
 ########## z-score ###########
-from sklearn.preprocessing import StandardScaler
+
 sc = StandardScaler()
 X= sc.fit_transform(X)
 
 #########################################################################
 ##################  Logistic Regression Classifier ######################
 
-from sklearn.linear_model import LogisticRegression
 clf = LogisticRegression()
-
 
 ###############  Model Evaluation using Cross Validation ################
 
-from sklearn.model_selection import cross_val_score
-import time
-
 tic = time.time()
 scores = cross_val_score(clf, X, y, cv=3)
 toc = time.time()
 T = toc-tic
 
 ###################  Step 2: Model Evaluation ######################
-from statistics import mean
+
 print('Accuracy Score using Cross validation k=3 is '+ str(mean(scores)))
 
 scores = cross_val_score(clf, X, y, cv=3, scoring='recall')
@@ -61,21 +66,17 @@ print('Recall Score using Cross validation k=3 is '+ str(mean(scores)))
 scores = cross_val_score(clf, X, y, cv=3, scoring='precision')
 print('Precision Score using Cross validation k=3 is '+ str(mean(scores)))
 
-
 ####################  Time Evaluation ##############################
 
 print('Logistic Regression classifier time '+' :' + str(T))
-
-
-
 
 ############################################################################################
 #############################  Naive bayes Classifier ######################################
-from sklearn.naive_bayes import GaussianNB
+
 clf =GaussianNB()
 
 ################### Step 1:  Model Construction #####################
-from sklearn.model_selection import cross_val_score
+
 import time
 
 tic = time.time()
@@ -84,7 +85,7 @@ toc = time.time()
 T = toc-tic
 
 ###################  Step 2: Model Evaluation ######################
-from statistics import mean
+
 print('Accuracy Score using Cross validation k=3 is '+ str(mean(scores)))
 
 scores = cross_val_score(clf, X, y, cv=3, scoring='recall')
@@ -93,21 +94,16 @@ print('Recall Score using Cross validation k=3 is '+ str(mean(scores)))
 scores = cross_val_score(clf, X, y, cv=3, scoring='precision')
 print('Precision Score using Cross validation k=3 is '+ str(mean(scores)))
 
-
 ####################  Time Evaluation ##############################
 
 print('Logistic Regression classifier time '+' :' + str(T))
-
-
 
 #################################################################################
 ##################  Random Forest Classifier ###################################
-from sklearn.ensemble import RandomForestClassifier
+
 clf =RandomForestClassifier()
 
 ################### Step 1:  Model Construction #####################
-from sklearn.model_selection import cross_val_score
-import time
 
 tic = time.time()
 scores = cross_val_score(clf, X, y, cv=3)
@@ -115,7 +111,7 @@ toc = time.time()
 T = toc-tic
 
 ###################  Step 2: Model Evaluation ######################
-from statistics import mean
+
 print('Accuracy Score using Cross validation k=3 is '+ str(mean(scores)))
 
 scores = cross_val_score(clf, X, y, cv=3, scoring='recall')
@@ -124,32 +120,24 @@ print('Recall Score using Cross validation k=3 is '+ str(mean(scores)))
 scores = cross_val_score(clf, X, y, cv=3, scoring='precision')
 print('Precision Score using Cross validation k=3 is '+ str(mean(scores)))
 
-
 ####################  Time Evaluation ##############################
 
 print('Logistic Regression classifier time '+' :' + str(T))
 
-
-
-
-
 ####################################################################################
 ########################  Artificial Neural Network ################################
-from sklearn.neural_network import MLPClassifier
+
 clf = MLPClassifier(solver='adam', hidden_layer_sizes=(20, 10))
 
 ################### Step 1:  Model Construction #####################
 
-from sklearn.model_selection import cross_val_score
-import time
-
 tic = time.time()
 scores = cross_val_score(clf, X, y, cv=3)
 toc = time.time()
 T = toc-tic
 
 ###################  Step 2: Model Evaluation ######################
-from statistics import mean
+
 print('Accuracy Score using Cross validation k=3 is '+ str(mean(scores)))
 
 scores = cross_val_score(clf, X, y, cv=3, scoring='recall')
@@ -161,3 +149,4 @@ print('Precision Score using Cross validation k=3 is '+ str(mean(scores)))
 ####################  Time Evaluation ##############################
 
 print('Logistic Regression classifier time '+' :' + str(T))
+
